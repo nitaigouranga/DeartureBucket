4a17c917f52a3bc83f42194a2f2c9c8293d7499f
nitaigouranga <anitha.kudupudi@gainwelltechnologies.com>
2023-08-01 16:20:01 -0500

    t

--- provider.tf
+++ provider.tf
@@ -1,12 +1,9 @@
 provider "aws" {
    region="us-east-1"
-   access_key = "AKIARNIR6AROFUW2LDFU"
-   secret_key = "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"
-}
+   }
 provider "aws" {
       region = "us-east-1"
-      alias = "cross_account"
-      assume_role{
+       assume_role{
       role_arn = "arn:aws:iam::182663769864:role/sftpmanagementaccountrole"
    }
 }
